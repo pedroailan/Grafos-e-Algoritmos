a9d1d2f402f45b7045110551844c1a66da060c8b
Ailan <pedroaso@dcomp.ufs.br>
2022-03-12 17:53:53 -0300

    aletreações main

--- grafos/main.py
+++ grafos/main.py
@@ -23,15 +23,13 @@ g.insere_aresta("3", "10")
 g.insere_aresta("11", "2")
 g.insere_aresta("11", "9")
 g.insere_aresta("11", "10")
-g.insere_aresta("8", "9")
+
 
 print("----------- Grafo 1 --------------")
+g.imprime()
 print("DFS")
 g.dfs()
 v = g.listar_vertice_sem_arcos_de_entrada()
-# for i in verticesSemArcosDeEntrada:
-#     travessia = g.busca_profundidade(i, [])
-#     print(f"Travessia para {i}: {travessia}")
 
 print("\nKahn")
 travessia = g.kahn(v)
@@ -66,13 +64,42 @@ g2.insere_aresta("GRAVATA", "PALETO")
 g2.insere_aresta("MEIAS", "SAPATOS")
 
 print("----------- Grafo 2 --------------")
+g2.imprime()
 print("DFS")
 g2.dfs()
 v = g2.listar_vertice_sem_arcos_de_entrada()
-# for i in verticesSemArcosDeEntrada:
-#     travessia = g2.busca_profundidade(i, [])
-#     print(f"Travessia para {i}: {travessia}")
 
 print("\nKahn")
 travessia = g2.kahn(v)
+print(travessia)
+print()
+
+
+g3 = m.Grafo(False, True)
+
+l2 = [
+    "1",
+    "1",
+    "3",
+    "4",
+    "5",
+]
+
+for i in l2:
+    g3.insere_vertice(i)
+
+g3.insere_aresta("1", "2")
+g3.insere_aresta("1", "4")
+g3.insere_aresta("2", "2")
+g3.insere_aresta("1", "5")
+g3.insere_aresta("5", "3")
+
+print("----------- Grafo 3 --------------")
+g3.imprime()
+print("DFS")
+g3.dfs()
+v = g3.listar_vertice_sem_arcos_de_entrada()
+
+print("\nKahn")
+travessia = g3.kahn(v)
 print(travessia)
